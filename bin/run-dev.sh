53850136a384db6b70bd0b5677ba24a58b1f8d12
Gavin Kalikapersaud <gavinkalika@gmail.com>
2020-12-06 11:36:35 -0500

    UI work (#13)
    * wip
    
    * wip
    
    * index ui
    
    * ui changes
    
    * wip
    
    * more ui work
    
    * more work
    
    * new section
    
    * more ui work
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- bin/run-dev.sh
+++
@@ -1,2 +0,0 @@
-source ../venv/bin/activate
-FLASK_APP=../main.py FLASK_ENV=development EXPLAIN_TEMPLATE_LOADING=true flask run
---
+++ run-dev.sh
@@ -0,0 +1,2 @@
+source activate
+FLASK_APP=main.py FLASK_ENV=development EXPLAIN_TEMPLATE_LOADING=true flask run

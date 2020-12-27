b498775e3e0e8e4171d5a57be8a1264d773ddc82
Gavin Kalikapersaud <gavinkalika@gmail.com>
2020-12-27 15:52:27 -0500

    Timer back end auth (#22)

--- app/repository/FastLoader.py
+++ app/repository/FastLoader.py
@@ -1,10 +1,16 @@
+from mysql.connector import MySQLConnection
+
+from app.repository.UserLoader import UserLoader
+
+
 class FastLoader:
 
-    def __init__(self, db_conn):
+    def __init__(self, db_conn: MySQLConnection, user_loader: UserLoader):
         """Constructor to get list of fasts."""
+        self.user_loader = user_loader
         self.db_conn = db_conn
 
-    def load_all(self):
+    def load_all(self, email: str) -> tuple:
         """Use this method to load all fasts
 
         Returns:
@@ -13,7 +19,25 @@ class FastLoader:
 
         db_cursor = self.db_conn.cursor()
 
+        self.user_loader.load_user_by_email(email)
+
         sql = "SELECT created_time, end_time FROM fast"
+        db_cursor.execute(sql)
+
+        return db_cursor.fetchall()
+
+    def load_all_that_have_no_end_time(self, email: str) -> tuple:
+        """Use this method to load all fasts
+
+        Returns:
+        array: List of fasts
+        """
+
+        db_cursor = self.db_conn.cursor()
+
+        user_id = self.user_loader.load_user_id_by_email(email)
+
+        sql = "SELECT created_time, end_time, user_id, id  FROM fast WHERE end_time is null AND user_id = {0}".format(user_id)
         db_cursor.execute(sql)
 
         return db_cursor.fetchall()
